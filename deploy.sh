14062bb52504022d8d91681eaddf55b97ec5355c
gojohill <gojohill@gmail.com>
2019-02-24 17:59:44 -0500

    changed travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Installing 'node-cmd'"
-echo npm install node-cmd
+npm install node-cmd
 echo "Installing 'node-ssh'"
-echo npm install node-ssh
+npm install node-ssh
 echo "Running 'deploy.js'"
 node deploy.js
